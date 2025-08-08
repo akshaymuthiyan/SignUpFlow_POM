9200d81ddb5cef92455be2381547086731084391
akshaymuthiyan <66552870+akshaymuthiyan@users.noreply.github.com>
2025-08-08 23:09:11 +0530

    DataProviders.java

--- src/test/java/utilities/DataProviders.java
+++ src/test/java/utilities/DataProviders.java
@@ -6,36 +6,27 @@ import org.testng.annotations.DataProvider;
 
 public class DataProviders {
 
-	//DataProvider 1
 
 	@DataProvider(name="LoginData")
 	public String [][] getData() throws IOException
 	{
-		String path=".\\testData\\SignUp_Data.xlsx";	//taking xl file from testdata
+		String path=".\\testData\\SignUp_Data.xlsx";	
 
-		ExcelUtility xlutil=new ExcelUtility(path);	//creating an object for XLUtility
+		ExcelUtility xlutil=new ExcelUtility(path);	
 
 		int totalrows=xlutil.getRowCount("Sheet1");
 		int totalcols=xlutil.getCellCount("Sheet1",1);
 
-		String logindata[][]=new String[totalrows][totalcols];	//created for two dimensional array which can stored
+		String logindata[][]=new String[totalrows][totalcols];	
 
-		for(int i=1;i<=totalrows;i++)	//1		//read the data from xl storing in two dimensional array
+		for(int i=1;i<=totalrows;i++)	
 		{
-			for(int j=0;j<totalcols;j++)	//0 	i is rows j is col
+			for(int j=0;j<totalcols;j++)	
 			{
-				logindata[i-1][j]=xlutil.getCellData("Sheet1", i, j);	//1,0
+				logindata[i-1][j]=xlutil.getCellData("Sheet1", i, j);	
 			}
 		}
 		return logindata;//returning two dimensions array
 
 	}
-
-	//DataProvider2
-
-	//DataProvider3
-
-	//DataProvider4
-
-
 }
